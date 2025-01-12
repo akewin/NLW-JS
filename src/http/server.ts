d2c1ac7a20801be583081233a405b92758412709
akewin <kewintk4@gmail.com>
2025-01-11 23:29:50 -0300

    new lecture

--- src/http/server.ts
+++ src/http/server.ts
@@ -10,9 +10,14 @@ import { createGoalRoute } from './routes/create-goal'
 import { createCompletionRoute } from './routes/create-completion'
 import { getPendingGoalsRoute } from './routes/get-pending-goals'
 import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
 
 // Create an instance of a Fastify application
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 // register the routes
 app.register(createGoalRoute)
